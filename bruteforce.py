8b3e6822ccea4efaec1416073abe5eb87e1ae034
Raian <raian.guimaraes@hotmail.com>
2025-02-15 22:41:47 -0300

    adicionado brute force

--- bruteforce.py
+++ bruteforce.py
@@ -1,14 +1,22 @@
-codigoGerado = 111112222233333
-codigoGiftCard = 111112222233333
-
- 
+def gerarCodigoManual(tamanho, caracteres='0123456789'):
+    if tamanho == 0:
+        yield ""  
+    else:
+        for caractere in caracteres:
+            for codigo in gerarCodigoManual(tamanho - 1, caracteres):
+                yield caractere + codigo 
+                
 def verificarCodigoGiftCard(codigoGerado,codigoGiftCard):
     if codigoGerado == codigoGiftCard:
-        print("Código Encontrado")
+        print("Código Encontrado: " + codigoGerado)
         return True
-    else:
-        print("Código Inválido")
-        return False
         
+def bruteForceGiftCard(codigoGiftCard, tamanho):
+    for codigoGerado in gerarCodigoManual(tamanho):
+        if verificarCodigoGiftCard(codigoGerado, codigoGiftCard):
+            return codigoGerado
 
-verificarCodigoGiftCard(codigoGerado,codigoGiftCard)
+# Exemplo de uso
+codigoGiftCard = "12345678"
+tamanhoCodigo = len(codigoGiftCard)
+bruteForceGiftCard(codigoGiftCard, tamanhoCodigo)
